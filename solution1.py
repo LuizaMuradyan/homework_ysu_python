11b9288774de093e67fc16b3d3f063a0496f01f5
LuizaMuradyan <124360779+LuizaMuradyan@users.noreply.github.com>
2023-02-14 21:26:06 +0400

    Add files via upload

---
+++ solution1.py
@@ -0,0 +1,12 @@
+# --------------- task N1 ------------------
+# import sys
+# print(sum([int(i) for  i in sys.argv[1]]))
+# --------------- task N2 ------------------
+# import sys
+# n = int(sys.argv[1])
+# x = 1
+# for i in range(n - 1, -1, -1):
+#     print('|' + ' ' * i + '#' * x )
+#     x += 1
+#---------------- task N3 ------------------ 
+#''' ? '''
